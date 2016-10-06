36122ad3901715d0176336871815f25a269cc377
Loh Kwai Hee <lohkwaihee@yahoo.com>
2016-10-06 17:29:55 +0800

    style welcome

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20161006044757) do
-
+ActiveRecord::Schema.define(version: 20161006073711) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
