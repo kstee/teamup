d0e6618ef3cec75bd1b32174009dff6e0270054c
gat41 <gloria.tengaihui@gmail.com>
2016-10-06 19:24:21 +0800

    rename column type in activities model to activity_type because type is pre-defined name and using this term as a column name will cause errors

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+#require 'faker'
+
+puts "Seeding table activities"
+
+Activity.create(type: "Futsal", description: "A form of indoor soccer with a small and heavier ball than normal.")
+
+Activity.create(type: "Badminton", description: "Hitting a birdie over a net with a long-necked racket. Doesn't sound very hard, does it?") 
+
+Activity.create(type: "Basketball", description: "A game played between two teams of five players in which goals are scored by throwing a ball through a netted hoop fixed above each end of the court.")
+
+Activity.create(type: "Hiking", description: "Walk for a long distance, especially across country or in the woods.")
+
+Activity.create(type: "Sepak Takraw", description: "A game originating in South Asia in which two teams kick a ball back and forth over a net")
+
+Activity.create(type: "Board Games", description: "Any game that comes in a box that you can play (eg. games with cards, games with a board, random pieces, etc.")
