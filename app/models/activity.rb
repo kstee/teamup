65778e27c01425b9e4198546296a737013769e20
Tee Kuang Seng <kstee_90@hotmail.com>
2016-10-07 14:57:04 +0800

    modify user model and activity model

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,7 +1,7 @@
 class Activity < ActiveRecord::Base
 
   #searchkick match: :word_start, searchable: [:listing_name]
-  has_many :user_activity
+  # has_many :user_activities
   has_many :users, through: :user_activity, dependent: :destroy
   has_many :listings, dependent: :destroy
 
