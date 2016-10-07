9ca9b5141d435d3198c21595138a88c41bab39cd
gat41 <gloria.tengaihui@gmail.com>
2016-10-07 17:59:49 +0800

    yet to solve

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,18 +8,20 @@ class User < ActiveRecord::Base
   enum gender: { "unknown" => 0, male: 1, female: 2, na: 9 } #if in symbol will also be converted to string
   mount_uploaders :photos, PhotoUploader
 
-  has_many :user_activity
-  has_many :user_listings
-  has_many :listings, dependent: :destroy
-  has_many :activities, through: :user_activity, dependent: :destroy
+  has_many :user_activity #this model is for user profile to state their fav activity/skill level/etc
+  has_many :activities, through: :user_activity, dependent: :destroy 
+
+  has_many :listings, dependent: :destroy #this model is for users who created a listing
+
+  has_many :user_listings #this model is for users who joined a listing
+  
 
   def self.from_omniauth(auth)
-    byebug
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[8,20]
       user.name = auth.info.name   # assuming the user model has a name
-      user.remote_photos_urls[0] = auth.info.image # assuming the user model has an image
+      user.photos_urls[0] = auth.info.image # assuming the user model has an image
     end
   end
 
